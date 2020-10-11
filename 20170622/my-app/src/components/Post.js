57efba4c2f01772657e1cfc67621018b41afd6ec
marcelosperalta <marcelosperalta@gmail.com>
2020-10-11 17:18:19 +0200

    rocketseat

--- 20170622/my-app/src/components/Post.js
+++ 20170622/my-app/src/components/Post.js
@@ -9,16 +9,28 @@ export default class Post extends React.Component {
 
         this.state = {
             comments: [
-                { text: "Bom post" }
+                { text: "Bom post!" }
             ]
         };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleSubmit(e) {
+        //this.setState
+        e.preventDefault();
+    }
+
 
     render() {
         return (
             <div>
                 {/* <h2>Post</h2> // cenário 1 */}
                 <h2>{this.props.title}</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <input />
+                    <button type="submit">Comentar</button>
+                </form>
                 {/* <Comment /> // cenário 2 */}
                 {/* <Comment text="Bom post" /> // cenário 3 */}
                 { this.state.comments.map((comment, index) => {
