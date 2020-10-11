53803f330b2b55ba7cbf9df497d48cfd6045298b
marcelosperalta <marcelosperalta@gmail.com>
2020-10-11 17:13:29 +0200

    rocketseat

--- 20170622/my-app/src/components/Post.js
+++ 20170622/my-app/src/components/Post.js
@@ -3,13 +3,27 @@ import React from "react";
 import Comment from "./Comment"
 
 export default class Post extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            comments: [
+                { text: "Bom post" }
+            ]
+        };
+    }
+
     render() {
         return (
             <div>
                 {/* <h2>Post</h2> // cenário 1 */}
                 <h2>{this.props.title}</h2>
                 {/* <Comment /> // cenário 2 */}
-                <Comment text="Bom post" />
+                {/* <Comment text="Bom post" /> // cenário 3 */}
+                { this.state.comments.map((comment, index) => {
+                    return <Comment key={index} text={comment.text} />
+                }) }
             </div>
         )
     }
