e0ad232ac3b784b6e1edf4c2be866ba6b04fa4cd
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2021-04-18 22:29:53 +0200

    rocketseat

--- maratona_discover_2021_02/src/db/init.js
+++ maratona_discover_2021_02/src/db/init.js
@@ -17,6 +17,18 @@
 // "http://github.com/marcelosperalta.png"
 // ...
 
+// ************************************ //
+//                EXECUTE               //
+//                                      //
+// cd src/db                            //
+// node init.js                         //
+//                                      //
+// or                                   //
+//                                      //
+// npm run init-db                      //
+//                                      //
+// ************************************ //
+
 const Database = require('config')
 
 // open database
@@ -27,11 +39,11 @@ Database.exec(`
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
         avatar TEXT,
-        monthly-budget INT,
-        days-per-week INT,
-        hours-per-day INT,
-        vatation-per-year INT,
-        costs-per-hour INT
+        monthly_budget INT,
+        days_per_week INT,
+        hours_per_day INT,
+        vatation_per_year INT,
+        costs_per_hour INT
     )
 `);
 
@@ -39,9 +51,57 @@ Database.exec(`
     CREATE TABLE jobs (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
-        daily-hours INT,
-        total-hours INT,
+        daily_hours INT,
+        total_hours INT,
         created_at DATETIME
+    )
+`);
+
+Database.run(`
+    INSERT INTO profile (
+        name,
+        avatar,
+        monthly_budget,
+        days_per_week,
+        hours_per_day,
+        vacation_per_year,
+        costs_per_hour
+    ) VALUES (
+        "Marcelo",
+        "http://github.com/marcelosperalta.png",
+        3000,
+        5,
+        5,
+        4,
+        75
+    )
+`);
+
+Database.run(`
+    INSERT INTO jobs (
+        name,
+        daily_hours,
+        total_hours,
+        created_at
+    ) VALUES (
+        "Project 1",
+        2,
+        2,
+        1618773898919
+    )
+`);
+
+Database.run(`
+    INSERT INTO jobs (
+        name,
+        daily_hours,
+        total_hours,
+        created_at
+    ) VALUES (
+        "Project 2",
+        3,
+        47,
+        1618773977594
     )
 `);
 
