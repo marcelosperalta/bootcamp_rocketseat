edaad2713994d857e967ed94db6231d93f3f39bf
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2021-04-20 11:21:40 +0200

    rocketseat

--- maratona_discover_2021_02/src/db/init.js
+++ maratona_discover_2021_02/src/db/init.js
@@ -29,81 +29,91 @@
 //                                      //
 // ************************************ //
 
-const Database = require('config')
+const Database = require('./config')
 
-// open database
-Database()
+const initDb = {
 
-Database.exec(` 
-    CREATE TABLE profile (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        avatar TEXT,
-        monthly_budget INT,
-        days_per_week INT,
-        hours_per_day INT,
-        vatation_per_year INT,
-        costs_per_hour INT
-    )
-`);
+    async init() {
 
-Database.exec(`
-    CREATE TABLE jobs (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        daily_hours INT,
-        total_hours INT,
-        created_at DATETIME
-    )
-`);
+        // open database
+        const db = await Database()
 
-Database.run(`
-    INSERT INTO profile (
-        name,
-        avatar,
-        monthly_budget,
-        days_per_week,
-        hours_per_day,
-        vacation_per_year,
-        costs_per_hour
-    ) VALUES (
-        "Marcelo",
-        "http://github.com/marcelosperalta.png",
-        3000,
-        5,
-        5,
-        4,
-        75
-    )
-`);
+        await db.exec(` 
+            CREATE TABLE profile (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                avatar TEXT,
+                monthly_budget INT,
+                days_per_week INT,
+                hours_per_day INT,
+                vacation_per_year INT,
+                costs_per_hour INT
+            )
+        `);
 
-Database.run(`
-    INSERT INTO jobs (
-        name,
-        daily_hours,
-        total_hours,
-        created_at
-    ) VALUES (
-        "Project 1",
-        2,
-        2,
-        1618773898919
-    )
-`);
+        await db.exec(`
+            CREATE TABLE jobs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                daily_hours INT,
+                total_hours INT,
+                created_at DATETIME
+            )
+        `);
 
-Database.run(`
-    INSERT INTO jobs (
-        name,
-        daily_hours,
-        total_hours,
-        created_at
-    ) VALUES (
-        "Project 2",
-        3,
-        47,
-        1618773977594
-    )
-`);
+        await db.run(`
+            INSERT INTO profile (
+                name,
+                avatar,
+                monthly_budget,
+                days_per_week,
+                hours_per_day,
+                vacation_per_year,
+                costs_per_hour
+            ) VALUES (
+                "Marcelo",
+                "http://github.com/marcelosperalta.png",
+                3000,
+                5,
+                5,
+                4,
+                75
+            )
+        `);
 
-// close database
-Database.close()
+        await db.run(`
+            INSERT INTO jobs (
+                name,
+                daily_hours,
+                total_hours,
+                created_at
+            ) VALUES (
+                "Project 1",
+                2,
+                2,
+                1618773898919
+            )
+        `);
+
+        await db.run(`
+            INSERT INTO jobs (
+                name,
+                daily_hours,
+                total_hours,
+                created_at
+            ) VALUES (
+                "Project 2",
+                3,
+                47,
+                1618773977594
+            )
+        `);
+
+        // close Database
+        await db.close()
+
+    }
+
+}
+
+initDb.init()
