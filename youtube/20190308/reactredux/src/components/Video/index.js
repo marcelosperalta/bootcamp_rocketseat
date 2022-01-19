49332c18b44f206d22fa3e9ea130948fefc69fa0
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2022-01-19 15:46:46 +0100

    rocketseat

--- youtube/20190308/reactredux/src/components/Video/index.js
+++ youtube/20190308/reactredux/src/components/Video/index.js
@@ -1,12 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Video extends Component {
-    render() {
-        return (
-            <div>
-                <strong>Module X</strong>
-                <span>Class X</span>
-            </div>
-        )
-    }
-}
+import { connect } from "react-redux";
+
+const Video = ({ activeModule, activeLesson }) => (
+    <div>
+        <h2>Module: <i>{activeModule.title}</i></h2>
+        <h3>Class: <i>{activeLesson.title}</i></h3>
+    </div>
+)
+
+export default connect(state =>({
+    activeModule: state.activeModule,
+    activeLesson: state.activeLesson,
+}))(Video);
