fd01dc3f5d5722e5440d2ebc480f608726d25c9d
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2022-01-19 14:10:54 +0100

    rocketseat

--- youtube/20190308/reactredux/src/components/Sidebar/index.js
+++ youtube/20190308/reactredux/src/components/Sidebar/index.js
@@ -1,21 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Sidebar extends Component {
-    render() {
-        const { modules } = this.state
-        return (
-            <aside>
-                { modules.map(module => (
-                    <div key={module.id}>
-                        <strong>{module.title}</strong>
-                        <ul>
-                            { module.lessons.map(lesson => (
-                                <li key={lesson.id}>{lesson.title}</li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
-            </aside>
-        );
-    }
-}
+import { connect } from "react-redux";
+
+const Sidebar = ({ modules }) => (
+    <aside>
+        {modules.map(module => (
+            <div key={module.id}>
+                <strong>{module.title}</strong>
+                <ul>
+                    { module.lessons.map(lesson => (
+                        <li key={lesson.id}>{lesson.title}</li>
+                    ))}
+                </ul>
+            </div>
+        ))}
+    </aside>
+)
+
+export default connect(state => ({ modules: state }))(Sidebar);
