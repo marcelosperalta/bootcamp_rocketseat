39b804d475d6138e1d99333e55eeb9120ed06bbb
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2022-01-25 14:29:02 +0100

    rocketseat

--- youtube/20220124/reactproject/src/App.tsx
+++ youtube/20220124/reactproject/src/App.tsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import { Tweet } from "./components/Tweet";
+
+import "./App.css";
+
 function App() {
-  return <h1>Hello World</h1>
+  const [tweets, setTweets]  = useState<string[]>([
+    'Tweet 1',
+    'Tweet 2',
+    'Tweet 3',
+    'Tweet 4',
+  ]);
+
+  function createTweet(){
+    setTweets([...tweets, "Tweet 5"])
+  }
+
+  return (
+    <div>
+      {tweets.map(tweet => {
+        return <Tweet text={tweet} />
+      })}
+
+      <button 
+        onClick={createTweet}
+        style={{
+          backgroundColor: "#8257e6",
+          border: 0,
+          padding: "6px 12px",
+        }}
+      >
+        Add tweet
+      </button>
+    </div>
+  )
 }
 
 export default App
