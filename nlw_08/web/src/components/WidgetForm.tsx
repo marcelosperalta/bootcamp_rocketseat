6405b7ac563286a18b37bbc389b2646d5ae7f046
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2022-05-05 16:29:32 +0200

    rocketseat

--- nlw_08/web/src/components/WidgetForm.tsx
+++ nlw_08/web/src/components/WidgetForm.tsx
@@ -1,5 +1,40 @@
 import { CloseButton } from "./CloseButton";
 
+import bugImageUrl from "../assets/bug.svg";
+import ideaImageUrl from "../assets/idea.svg";
+import otherImageUrl from "../assets/other.svg";
+
+const feedbackTypes = {
+    BUG: {
+        title: "Problem",
+        image: {
+            source: bugImageUrl,
+            alt: "Bug icon"
+        },
+    },
+    IDEA: {
+        title: "Sugestion",
+        image: {
+            source: ideaImageUrl,
+            alt: "Lamp icon"
+        },
+    },
+    OTHER: {
+        title: "Other",
+        image: {
+            source: otherImageUrl,
+            alt: "Thought icon"
+        },
+    },
+}
+
+// Object.entries(feedbackTypes) => 
+// [ 
+//     ["BUG", {...}], 
+//     ["IDEA", {...}], 
+//     ["OTHER", {...}] 
+// ]
+
 export function WidgetForm() {
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -11,7 +46,20 @@ export function WidgetForm() {
                 <CloseButton />
             </header>
 
-            <p>Hello World</p>
+            <div className="flex py-8 gap-2 w-full">
+                { Object.entries(feedbackTypes).map(([key, value]) => {
+                    return (
+                        <button
+                            key={key}
+                            type="button"
+                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                        >
+                            <img src={value.image.source} alt={value.image.alt} />
+                            <span>{value.title}</span>
+                        </button>
+                    );
+                })}
+            </div>
 
             <footer className="text-xs text-neutral-400">
                 Made with ❤ by <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
