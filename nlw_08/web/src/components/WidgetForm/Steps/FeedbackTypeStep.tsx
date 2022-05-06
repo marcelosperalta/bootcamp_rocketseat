c75275bcc3835c7c85a7fb0d243e89c5dfa708ff
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2022-05-06 15:10:12 +0200

    rocketseat

--- nlw_08/web/src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ nlw_08/web/src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -1,3 +1,4 @@
+import { CloseButton } from "../../CloseButton";
 import { FeedbackType, feedbackTypes } from "..";
 
 interface FeedbackTypeStepProps {
@@ -7,21 +8,31 @@ interface FeedbackTypeStepProps {
 // export function FeedbackTypeStep(props: FeedbackTypeStepProps) {
 export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedbackTypeStepProps) {
     return (
-        <div className="flex py-8 gap-2 w-full">
-            { Object.entries(feedbackTypes).map(([key, value]) => {
-                return (
-                    <button
-                        key={key}
-                        type="button"
-                        className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                        // onClick={() => props.onFeedbackTypeChanged(key as FeedbackType)}
-                        onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
-                    >
-                        <img src={value.image.source} alt={value.image.alt} />
-                        <span>{value.title}</span>
-                    </button>
-                );
-            })}
-        </div>
+        <>
+            <header>
+                <span className="text-xl leading-6">
+                    Leave feedback
+                </span>
+
+                <CloseButton />
+            </header>
+
+            <div className="flex py-8 gap-2 w-full">
+                { Object.entries(feedbackTypes).map(([key, value]) => {
+                    return (
+                        <button
+                            key={key}
+                            type="button"
+                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                            // onClick={() => props.onFeedbackTypeChanged(key as FeedbackType)}
+                            onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
+                        >
+                            <img src={value.image.source} alt={value.image.alt} />
+                            <span>{value.title}</span>
+                        </button>
+                    );
+                })}
+            </div>
+        </>
     )
 }
--- nlw_08/web/src/components/WidgetForm/index.tsx
+++ nlw_08/web/src/components/WidgetForm/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import { CloseButton } from "../CloseButton";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 import bugImageUrl from "../../assets/bug.svg";
 import ideaImageUrl from "../../assets/idea.svg";
@@ -45,18 +46,10 @@ export function WidgetForm() {
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            <header>
-                <span className="text-xl leading-6">
-                    Leave feedback
-                </span>
-
-                <CloseButton />
-            </header>
-
             {!feedbackType ? (
                 <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
             ) : (
-                <p>Hello World</p>
+                <FeedbackContentStep feedbackType={feedbackType} />
             ) }
 
             <footer className="text-xs text-neutral-400">
