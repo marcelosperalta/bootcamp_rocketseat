8b36b6b4830a938f26cd28b3ae30f817215cd9db
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2022-05-11 16:23:03 +0200

    rocketseat

---
+++ nlw_08/server/src/routes.ts
@@ -0,0 +1,58 @@
+import express from 'express';
+import nodemailer from 'nodemailer';
+import { prisma } from './prisma';
+
+export const routes = express.Router()
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "f03cbb204ec3c7",
+      pass: "b146bcae390077"
+    }
+});
+
+// app.post('/feedbacks', (req, res) => {
+// app.post('/feedbacks', async (req, res) => {
+routes.post('/feedbacks', async (req, res) => {
+    // console.log(req.body);
+
+    // prisma.feedback.create({
+    //     data: {
+    //         type: req.body.type,
+    //         comment: req.body.comment,
+    //         screenshot: req.body.screenshot,
+    //     }
+    // })
+    const { type, comment, screenshot } = req.body;
+    // prisma.feedback.create({
+    //     data: {
+    //         type: type,
+    //         comment: comment,
+    //         screenshot: screenshot,
+    //     }
+    // })
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot,
+        }
+    })
+
+    await transport.sendMail({
+        from: "Feedget Team <hi@feedget.com>",
+        to: "Marcelo Peralta <marcelosperalta@gmail.com>",
+        subject: "New feedback",
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+                `<p><b>Feedback type:</b> ${type}<p>`,
+                `<p><b>Comment:</b> ${comment}`,
+            `</div>`,
+        ].join(''),
+    });
+
+    // return res.send("Hello World");
+    return res.status(201).json( { data: feedback } );
+})
--- nlw_08/server/src/server.ts
+++ nlw_08/server/src/server.ts
@@ -1,68 +1,17 @@
 import express from 'express';
-import nodemailer from 'nodemailer';
-import { prisma } from './prisma';
+
+import { routes } from './routes';
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
-
-const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "f03cbb204ec3c7",
-      pass: "b146bcae390077"
-    }
-});
-
-// app.get('/users', (req, res) => {
-//     return res.send("Hello World");
-// })
-
-// app.post('/feedbacks', (req, res) => {
-app.post('/feedbacks', async (req, res) => {
-    // console.log(req.body);
-
-    // prisma.feedback.create({
-    //     data: {
-    //         type: req.body.type,
-    //         comment: req.body.comment,
-    //         screenshot: req.body.screenshot,
-    //     }
-    // })
-    const { type, comment, screenshot } = req.body;
-    // prisma.feedback.create({
-    //     data: {
-    //         type: type,
-    //         comment: comment,
-    //         screenshot: screenshot,
-    //     }
-    // })
-    const feedback = await prisma.feedback.create({
-        data: {
-            type,
-            comment,
-            screenshot,
-        }
-    })
-
-    await transport.sendMail({
-        from: "Feedget Team <hi@feedget.com>",
-        to: "Marcelo Peralta <marcelosperalta@gmail.com>",
-        subject: "New feedback",
-        html: [
-            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-                `<p><b>Feedback type:</b> ${type}<p>`,
-                `<p><b>Comment:</b> ${comment}`,
-            `</div>`,
-        ].join(''),
-    });
-
-    // return res.send("Hello World");
-    return res.status(201).json( { data: feedback } );
-})
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 })
+
+// app.get('/users', (req, res) => {
+//     return res.send("Hello World");
+// })
